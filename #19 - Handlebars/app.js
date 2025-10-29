972aefc94e58bd0ce2acf5371a1a0d60001fda9a
pedrolbpaula-hash <pedrolbpaula@gmail.com>
2025-10-29 13:04:36 -0300

    Alterações pontuais no handlebars para melhorar o funcionamento

--- #19 - Handlebars/app.js
+++ #19 - Handlebars/app.js
@@ -1,28 +1,32 @@
-const express = require('express'); 
-const app = express(); 
-const handlebars = require('express-handlebars'); 
-const port = 5000; 
+const express = require('express');
+const { engine } = require('express-handlebars');
+const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
+
+const port = 5000;
 
 // Config 
-    // Template Engine 
-    app.engine('handlebars', handlebars({defaultLayout: 'main'})); 
-    app.set('view engine', 'handlebars'); 
-    // Conexão com o banco de dados MySql
-    const Sequelize = require('sequelize'); 
-    const sequelize = new Sequelize('teste', 'root', '997085977', {
-        host: "localhost", 
-        dialect: "mysql"
-    })
+// Template Engine 
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+// Conexão com o banco de dados MySql
+const sequelize = new Sequelize('teste', 'root', '997085977', {
+    host: "localhost",
+    dialect: "mysql"
+})
 
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Teste da Silva</h1>`); 
+    res.send(`<h1>Teste</h1>`);
 })
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Não foi possível localizar a sua página</h1>'); 
+    res.status(404).send('<h1>Não foi possível localizar a sua página</h1>');
 })
 
 app.listen(port, () => {
-    console.log(`Servidor está rodando na porta ${port}`); 
+    console.log(`Servidor está rodando na porta ${port}`);
 })
